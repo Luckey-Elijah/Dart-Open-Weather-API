b7993a4fd75e6da490c5c1a7838f6836f7691a42
Elijah Luckey <luckeyelijah112@gmail.com>
2021-01-07 11:53:32 -0500

    [current_weather] adds byCityName function

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,13 +1,60 @@
+import 'dart:io';
 import 'package:http/http.dart';
+import 'package:path/path.dart';
+import 'package:yaml/yaml.dart';
+import 'constants.dart' show currentWeatherBaseUrl;
 
-Future<dynamic> byCityName(
-  String city,
-  String baseUrl,
-  String apiKey, {
-  int stateCode,
-  int countryCode,
+/// Get CurrentWeather JSON string by by city name.
+Future<String> byCityName(
+  String city, {
+  String stateCode,
+  String countryCode,
+  String apiKey,
+  String baseUrl = currentWeatherBaseUrl,
 }) async {
-  var url = baseUrl + '?q=${city}&appid=${apiKey}';
-  var body = (await get(url)).body;
+  // Throw error on null city
+  if (city == null) {
+    throw ArgumentError('City name cannot be null.');
+  }
+
+  apiKey ??= getSecretKey('secrets.yaml');
+
+  stateCode = (stateCode == null) ? '' : ',${stateCode}';
+  countryCode = (countryCode == null) ? '' : ',${countryCode}';
+
+  var url =
+      baseUrl + '?q=${city}' + stateCode + countryCode + '&appid=${apiKey}';
+  var res = await get(url);
+  var body = '';
+
+  if (res.statusCode == 200) {
+    body = res.body;
+  } else {
+    throw ArgumentError('Made a bad request. Status code ${res.statusCode}.');
+  }
+
   return body;
+}
+
+/// Get CurrentWeather JSON string by by City ID
+Future<String> byCityId() {}
+
+Future<String> byCoordinates() {}
+
+Future<String> byZipCode() {}
+
+String getSecretKey(String path) {
+  try {
+    var file = File(path);
+    if (file?.existsSync() == true) {
+      var doc = loadYaml(file.readAsStringSync());
+      return doc['open_weather_api_key'] ?? (throw ArgumentError());
+    } else {
+      throw PathException('File does not exist.');
+    }
+  } catch (e) {
+    stderr.writeln(e);
+    stderr.writeln('Could not get API Key from `${path}` file.');
+    exit(1);
+  }
 }
