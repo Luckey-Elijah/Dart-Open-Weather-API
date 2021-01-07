dfc09308d27ed04142b38da04c978b6af4286d19
Elijah Luckey <luckeyelijah112@gmail.com>
2021-01-07 12:36:20 -0500

    [current_weather] adds byCityId method

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart';
 import 'package:path/path.dart';
@@ -17,13 +18,12 @@ Future<String> byCityName(
     throw ArgumentError('City name cannot be null.');
   }
 
-  apiKey ??= getSecretKey('secrets.yaml');
+  apiKey ??= getSecretKey();
 
   stateCode = (stateCode == null) ? '' : ',${stateCode}';
   countryCode = (countryCode == null) ? '' : ',${countryCode}';
 
-  var url =
-      baseUrl + '?q=${city}' + stateCode + countryCode + '&appid=${apiKey}';
+  var url = baseUrl + '?q=${city}' + stateCode + countryCode + '&appid=$apiKey';
   var res = await get(url);
   var body = '';
 
@@ -37,13 +37,38 @@ Future<String> byCityName(
 }
 
 /// Get CurrentWeather JSON string by by City ID
-Future<String> byCityId() {}
+Future<String> byCityId(
+  int cityId, {
+  String apiKey,
+  String baseUrl = currentWeatherBaseUrl,
+}) async {
+  if (cityId == null) {
+    throw ArgumentError('City ID cannot be null.');
+  }
 
+  apiKey ??= getSecretKey();
+
+  var url = baseUrl + '?id=$cityId&appid=$apiKey';
+  var res = await get(url);
+  var body = '';
+
+  if (res.statusCode == 200) {
+    body = res.body;
+  } else {
+    throw ArgumentError('Made a bad request. Status code ${res.statusCode}.');
+  }
+
+  return body;
+}
+
+// ignore: missing_return
 Future<String> byCoordinates() {}
 
+// ignore: missing_return
 Future<String> byZipCode() {}
 
-String getSecretKey(String path) {
+/// Fetches the API Key from the selected path.
+String getSecretKey({String path = 'secrets.yaml'}) {
   try {
     var file = File(path);
     if (file?.existsSync() == true) {
@@ -57,4 +82,15 @@ String getSecretKey(String path) {
     stderr.writeln('Could not get API Key from `${path}` file.');
     exit(1);
   }
+}
+
+/// Formats contents of a JSON body response [source] nicely.
+String prettyJson(String source) {
+  var map = json.decode(source);
+
+  var encoder = JsonEncoder.withIndent('  ');
+
+  var pretty = encoder.convert(map);
+
+  return pretty;
 }
